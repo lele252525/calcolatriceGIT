2c41ec097e5db24028ef59a918ce9f580947800f
DocenzaBegear <f.pilettamassaro@begear.it>
2024-11-18 15:12:04 +0100

    Primo commit

---
+++ src/it/begear/calcolatriceGIT/Main.java
@@ -0,0 +1,16 @@
+package it.begear.calcolatriceGIT;
+
+import it.ebegar.calcolatriceGIT.utility.Calcoli;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Calcolatrice GIT");
+
+        int a = 1;
+        int b = 2;
+
+        System.out.println("Somma: " + Calcoli.somma(a,b));
+
+    }
+}
---
+++ src/it/ebegar/calcolatriceGIT/utility/Calcoli.java
@@ -0,0 +1,10 @@
+package it.ebegar.calcolatriceGIT.utility;
+
+public class Calcoli {
+
+    //Base
+    public static int somma (int a, int b) {
+        return a+b;
+    }
+
+}
